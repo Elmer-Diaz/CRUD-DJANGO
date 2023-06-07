29a018c48c164291a550326488337f0e7aa9afe9
Elmer-Diaz <114196068+Elmer-Diaz@users.noreply.github.com>
2023-06-07 01:00:36 -0500

    eliminar

--- universidad/Aplicaciones/Academico/views.py
+++ universidad/Aplicaciones/Academico/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Curso
 # Create your views here.
 
 
 def home(request):
-    return render(request, "gestionCursos.html")
+    cursosListados = Curso.objects.all()
+    return render(request, "gestionCursos.html", {"cursos": cursosListados})
+
+
+def registrarCurso(request):
+    codigo=request.POST['txtCodigo']
+    nombre=request.POST['txtNombre']
+    creditos=request.POST['numCreditos']
+
+    curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    return redirect('/')
+
+def eliminarCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    curso.delete()
+    return redirect('/')
